7b3066183bdb2ea53f2280db392778a3122c834f
Evgeniya86 <81212751+Evgeniya86@users.noreply.github.com>
2021-05-10 19:20:05 +0400

    Add files via upload

---
+++ CRMCreateContactTestng.java
@@ -0,0 +1,84 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class CRMCreateContactTestng {
+    WebDriver driver;
+    WebDriverWait webDriverWait;
+    private static final String BASE_URL = "https://crm.geekbrains.space/";
+
+
+    @BeforeSuite
+    void setupDataBase() {
+        System.out.println("Before suite setup database");
+    }
+
+    @BeforeTest
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    void setUpBrowser() {
+        driver = new ChromeDriver();
+        webDriverWait = new WebDriverWait(driver, 5);
+        driver.get(BASE_URL);
+        login();
+    }
+
+    @Test(description = "Регистрация в crm", enabled = true)
+    void CreateContact() {
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.xpath("//span[text()='Контрагенты']/ancestor::a")))
+                .build()
+                .perform();
+        driver.findElement(By.xpath("//li[@data-route='crm_contact_index']/a")).click();
+        driver.findElement(By.xpath("//a[text()='Создать контактное лицо']")).click();
+
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='crm_contact[lastName]']")));
+
+
+        driver.findElement(By.xpath("//input[@name='crm_contact[lastName]']")).sendKeys("Норина");
+
+        driver.findElement(By.xpath("//input[@name='crm_contact[firstName]']")).sendKeys("Александра");
+
+        driver.findElement(By.xpath("//span[text()='Укажите организацию']/..")).click();
+
+        driver.findElement(By.xpath("//input[@class='select2-input select2-focused']")).sendKeys("1234");
+
+        driver.findElement(By.xpath("//input[@class='select2-input select2-focused']")).sendKeys(Keys.ENTER);
+
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='crm_contact[jobTitle]']")));
+        driver.findElement(By.xpath("//input[@name='crm_contact[jobTitle]']")).sendKeys("менеджер");
+
+        driver.findElement(By.xpath("//button[contains(text(),'Сохранить и закрыть')]")).click();
+
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated
+                (By.xpath("//*[text()='Контактное лицо сохранено']")));
+
+        driver.findElement(By.xpath("//*[text()='Контактное лицо сохранено']"));
+
+    }
+
+    private void login() {
+        driver.get(BASE_URL);
+        driver.findElement(By.id("prependedInput")).sendKeys("Applanatest1");
+        driver.findElement(By.id("prependedInput2")).sendKeys("Student2020!");
+        driver.findElement(By.id("_submit")).click();
+    }
+    //@AfterMethod
+    //void closeBrowser() {
+    //driver.quit();
+    //}
+}
